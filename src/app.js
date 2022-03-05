a98ed668c148689c21896134cf3ab9ed5f6d32f4
Pierre <Pierre_gourgouillon@hotmail.com>
2022-03-05 19:01:32 +0100

    install cors

--- src/app.js
+++ src/app.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const authMiddleware = require('./middlewares/auth')
 
 const eventRoutes = require('./routes/event');
-const authRoutes = require('./routes/auth')
-
-mongoose.connect(
-    "mongodb+srv://pierre:pierre@cluster0.ikion.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true,
-        useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch((e) => console.log(e));
+const authRoutes = require('./routes/auth');
+const connectionMongoDB = require('./mongo/connection');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
 
+connectionMongoDB()
+
+app.use(cors());
 app.use(bodyParser.json())
 
 app.use('/api/auth', authMiddleware, authRoutes);
--- src/routes/event.js
+++ src/routes/event.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/event')
 
-router.get('/' + '', eventController.getEvents);
+router.get('/', eventController.getEvents);
 router.get('/:eventId', eventController.getEventById);
 router.post('/', eventController.createEvent);
 router.put('/:eventId', eventController.modifyEvent);
