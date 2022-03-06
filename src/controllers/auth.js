014c64053db1900321c48240d0cff34271f7c212
Pierre <Pierre_gourgouillon@hotmail.com>
2022-03-06 19:40:50 +0100

    Change auth response

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -7,7 +7,10 @@ exports.registerUser = async (req, res, next) => {
     })
     try {
         await user.save()
-        res.status(201).json({ message: 'Post saved successfully!' });
+        res.status(201).json({
+            error: null,
+            data: 'Post saved successfully!'
+        });
     } catch (error) {
         res.status(400).json({ error: error });
     }
@@ -16,8 +19,14 @@ exports.registerUser = async (req, res, next) => {
 exports.me = async (req, res, next) => {
     try {
         const user = await User.findOne({ firebaseId: req.user.firebaseId })
-        res.status(200).json({ data: user })
+        res.status(200).json({
+            error: null,
+            data: user
+        })
     } catch (error) {
-        res.status(404).json({ error: "NOT_FOUND" })
+        res.status(404).json({
+            error: "NOT_FOUND",
+            data: null
+        })
     }
 }
