8e0c478b96ed3309c8a20971aa819954d477aece
Debian <debian@vps-2cf5c70c.vps.ovh.net>
2022-04-11 18:52:01 +0000

    modify route event

--- src/routes/event.js
+++ src/routes/event.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/event')
 
-
 router.get('/', eventController.getEvents);
 router.get('/:eventId', eventController.getEventById);
 router.post('/', eventController.createEvent);
 router.put('/:eventId', eventController.modifyEvent);
+router.post("/:eventId/members/add", eventController.addMembers);
+router.delete("/:eventId/members/delete", eventController.deleteMembers);
 router.delete('/:eventId', eventController.deleteEvent);
-router.post("/:eventId/addMembers", eventController.addMembers)
-router.delete("/:eventId/deleteMembers", eventController.deleteMembers)
 
 module.exports = router;
